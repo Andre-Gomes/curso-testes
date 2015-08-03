c06c5b60bf703e6ea85c1abbd51d4c08704b1eb5
André Gomes de Sousa - Dataprev/PB <andre.gsousa@dataprev.gov.br>
2015-08-03 17:54:22 -0300

    Classe de testes

---
+++ Teste.java
@@ -0,0 +1,9 @@
+public class Teste{
+
+	public static void main(String[] args){
+		System.out.println("Classe de teste 1");
+		System.out.println("Classe de teste 2");
+		System.out.println("Classe de teste 3");
+	}
+
+}
